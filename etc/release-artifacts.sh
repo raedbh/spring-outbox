2d58cc52b52017f24bf309d2ee27af0095acf144
Raed <raedbh@gmail.com>
2025-01-13 00:39:52 +0100

    GH-20 - Add dedicated release script excluding connectors from being published to Maven Central.

---
+++ etc/release-artifacts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MODULES=$(mvn help:evaluate -Dexpression=project.modules -q -DforceStdout | \
+sed -n '/<string>/ s/.*<string>\(.*\)<\/string>.*/\1/p' | \
+grep -v -E '^(spring-outbox-debezium-connector.*|spring-outbox-sample|spring-outbox-integration-tests)$' | \
+tr '\n' ',' | \
+sed 's/,$//')
+
+if [ -z "$MODULES" ]; then
+  echo "No modules found to build!"
+  exit 1
+fi
+
+./mvnw -Prelease deploy -s settings.xml  -pl "$MODULES" -am -DskipTests
+
+exit $?
